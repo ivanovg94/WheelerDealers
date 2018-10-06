1c3b920c2b9cd29736aedfb4e0ae9c048f6c150e
Viktor Doninski <vicktord@mail.bg>
2018-10-06 14:17:11 +0300

    something changed :D

--- Dealership/Dealership.Client/Core/DealershipEngine.cs
+++ Dealership/Dealership.Client/Core/DealershipEngine.cs
@@ -19,6 +19,7 @@ namespace Dealership.Client.Core
         }
         public void Run()
         {
+
             while ((input = Console.ReadLine()) != "exit")
             {
                 var inputParams = input.Split();
