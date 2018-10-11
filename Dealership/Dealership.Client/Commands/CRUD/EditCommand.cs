428a669b54db405e724ff37ca492b7fdb9f945f5
Viktor Doninski <vicktord@mail.bg>
2018-10-11 15:40:48 +0300

    refactor_editCommnds

--- Dealership/Dealership.Client/Commands/CRUD/EditCommand.cs
+++ Dealership/Dealership.Client/Commands/CRUD/EditCommand.cs
@@ -13,26 +13,25 @@ namespace Dealership.Client.Commands.CRUD
     {
         public ICarService CarService { get; set; }
 
-        // edit [id]
+        // edit [exact property.Name] [id] [newValue] 'optional => [secondNewValue]'
         public override string Execute(string[] parameters)
         {
             var prop = parameters[0];
             var id = parameters[1];
 
-            string secondNewValue = "";
+            //string secondNewValue = "";
 
-            if (parameters.Count() == 4)
-            {
-                secondNewValue = parameters[3];
+            //if (parameters.Count() == 4)
+            //{
+            //    secondNewValue = parameters[3];
+            //}
 
-            }
-            //get CarService public methods
             var methods = this.CarService.GetType().GetMethods();
 
             foreach (var method in methods)
-            {// find adequate method
+            {
                 if (method.Name.Contains("Edit" + prop))
-                { // invoke method with needed parameters
+                {
                     method.Invoke(CarService, new object[] { parameters.Skip(1).ToArray() });
                     break;
                 }
