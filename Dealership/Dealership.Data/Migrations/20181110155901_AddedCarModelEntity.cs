e569ac16cb2fbfe4d62a71a2d9d482adefd3d7b2
Martin Kaerski <kaerski@gmail.com>
2018-11-10 17:59:50 +0200

    Fixed logo button, added carModel : Entity

---
+++ Dealership/Dealership.Data/Migrations/20181110155901_AddedCarModelEntity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dealership.Data.Migrations
+{
+    public partial class AddedCarModelEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "CarModels",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "CarModels",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "CarModels",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "CarModels",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "BodyTypes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "SUV");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "CarModels");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "CarModels");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "CarModels");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "CarModels");
+
+            migrationBuilder.UpdateData(
+                table: "BodyTypes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "Suv");
+        }
+    }
+}
